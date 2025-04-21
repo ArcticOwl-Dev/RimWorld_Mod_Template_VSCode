92ab9c5390ff068a8ce05af9de589f9b25b5e136
AllenCaine <56565659+AllenCaine@users.noreply.github.com>
2025-04-21 20:23:42 +0200

    Fix target framework version in project file and clean up unused using directives in Main.cs

--- src/Main.cs
+++ src/Main.cs
@@ -1,21 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-
-using UnityEngine;
+﻿
 using Verse;
-using Verse.AI;
-using Verse.AI.Group;
-using Verse.Sound;
-using Verse.Noise;
-using Verse.Grammar;
-using RimWorld;
-using RimWorld.Planet;
 
 // *Uncomment for Harmony*
-// using System.Reflection;
 // using HarmonyLib;
 
 namespace Template
